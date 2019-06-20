933176a716b7594d2385dd951d2b3d6c595c88ee
Mohit Negi <mohitsngh1997@gmail.com>
2019-06-20 11:59:12 +0530

    add alertmodal to the quake app

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+Map _data;
+List _features;
 void main() async {
-  Intl.defaultLocale = 'pt_BR';
-  Map _data = await getJSON();
-  var format = new DateFormat("yMd");
-  var dateString = format.format(_data['features'][0]['properties']['time'] * 1000);
-  print(dateString);
+  // Intl.defaultLocale = 'pt_BR';
+  _data = await getJSON();
+  _features = _data['features'];
+  // var format = new DateFormat("yMd");
+  // var dateString = format.format(_data['features'][0]['properties']['time'] * 1000);
+  // print(dateString);
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
@@ -16,30 +19,68 @@ void main() async {
         title: Text("Quake"),
       ),
       body: ListView.builder(
-        itemCount: _data['features'].length,
+        itemCount: _features.length,
+        padding: const EdgeInsets.all(15.0),
         itemBuilder: (BuildContext context, int index) {
+          var format = new DateFormat.yMMMMd('en_US').add_jm();
+
+          var date = format.format(DateTime.fromMicrosecondsSinceEpoch(
+              _features[index]['properties']['time'] * 1000,
+              isUtc: true));
           return Column(
             children: <Widget>[
               Divider(
                 height: 3.4,
               ),
-              // _data['features'][index]['properties']['mag']
               ListTile(
-                onTap: () => print('tapped'),
-                title: Text("fsfasdfas"),
-                subtitle: Text("${_data['features'][index]['properties']['place']}"),
-                leading: CircleAvatar(backgroundColor: Colors.greenAccent,child: Text('${_data['features'][index]['properties']['mag']}'),)
-              )
-            ],
-          );
-        },
-      ),
-    ),
-  ));
+                  onTap: () { _showAlertMessage(context, "${_features[index]['properties']['title']}") ;}, 
+                                    title: Text(
+                                      "$date",
+                                      style: TextStyle(
+                                          fontSize: 19.5,
+                                          fontWeight: FontWeight.w500,
+                                          color: Colors.orange),
+                                    ),
+                                    subtitle: Text("${_features[index]['properties']['place']}",
+                                        style: TextStyle(
+                                            fontSize: 14.5,
+                                            fontWeight: FontWeight.normal,
+                                            color: Colors.grey,
+                                            fontStyle: FontStyle.italic)),
+                                    leading: CircleAvatar(
+                                      backgroundColor: Colors.red,
+                                      child: Text('${_features[index]['properties']['mag']}',
+                                          style: TextStyle(
+                                              fontSize: 16.5,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.white,
+                                              fontStyle: FontStyle.normal)),
+                                    ))
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                    ));
+                  }
+                  
+                  void _showAlertMessage(BuildContext context, String message) {
+                    var alert = AlertDialog(
+                      title: Text("Quakes"),
+                      content: Text(message),
+                      actions: <Widget>[
+                        FlatButton(onPressed: (){Navigator.pop(context);},
+                          child: Text("OK"),
+                        )
+                      ],
+
+                    );
+                    showDialog(context: context, child: alert);
 }
 
 Future<Map> getJSON() async {
-  String APIURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+  String APIURL =
+      "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
   http.Response response = await http.get(APIURL);
   return json.decode(response.body);
 }
